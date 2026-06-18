3d0d8aaf10ab1e4e764be52ebe50be3d26810ea8
RICHIMPZ <richiemwebi@gmail.com>
2026-06-18 10:35:57 +0300

    updated typeof and consolelog file

--- JS/into/primitiveDataTypes.js
+++ JS/into/primitiveDataTypes.js
@@ -1,4 +1,3 @@
-//numbers
 
 let number1=34;
 let number2=45.43;
@@ -10,22 +9,3 @@ console.log(number2)
 console.log(number3)
 
 //strings
-
-let str1='single quotes';
-let str2="double quotes";
-let str3=`this is backticks`;
-console.log(str1)
-console.log(str2)
-console.log(str3)
-
-//boolean
-let isMarried=true;
-let isUnderage=false;
-console.log(isMarried)
-console.log(isUnderage)
-
-//null and undefined
-let myName=null;
-let bankBalance=undefined;
-console.log(myName)
-console.log(bankBalance)
---
+++ JS/into/typeOf.js
@@ -0,0 +1,17 @@
+let number1=4534;
+console.log("number1",number1,"it's type is",typeof number1)
+
+let striNumber="42444";
+console.log("striNumber",striNumber,"it's type is",typeof striNumber)
+
+let myName="John";
+console.log("number1",myName,"it's type is",typeof myName)
+
+let isSingle=false;
+console.log("isSingle",isSingle,"it's type is",typeof isSingle)
+
+let firstName=null;
+console.log("firstName",firstName,"it's type is",typeof firstName)
+
+let missingMark;
+console.log("missingMark",missingMark,"it's type is",typeof missingMark)
