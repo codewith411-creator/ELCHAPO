fc3c260eb250e827a9166cfe5a418578dde81149
RICHIMPZ <richiemwebi@gmail.com>
2026-06-23 10:29:26 +0300

    updated truthy and falsy

---
+++ JS/into/truthyAndFalsey.js
@@ -0,0 +1,10 @@
+let username1="john karangi";
+let username2=""
+console.log(username1,username2)
+
+let activeUserName=username2 || username1
+
+console.log(activeUserName)
+
+let bothUsername=username1&&username2
+console.log(bothUsername)
