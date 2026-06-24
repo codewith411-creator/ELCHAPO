2dc981a0fbfd3c98ae9838137ea7986aa581f49b
RICHIMPZ <richiemwebi@gmail.com>
2026-06-24 11:29:21 +0300

    added the ifOnly.js

---
+++ JS/into/ifOnly.js
@@ -0,0 +1,21 @@
+let age=prompt("enter your age:")
+alert(`you entered ${age}`)
+
+if(age>10){
+    alert("you are a baby")
+}
+if(age>13){
+    alert("you are a teen")
+}
+if(age>18){
+    alert("you can legally drink and smoke")
+}
+if(age>27){
+    alert("best age for marriage")
+}
+if(age>45){
+    alert("best age for retirement")
+}
+if(age>50){
+    alert("you are an ancestor")
+}
