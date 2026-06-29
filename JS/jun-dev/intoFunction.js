2a78b75a00c95d689b734a841df3db963d1c0417
RICHIMPZ <richiemwebi@gmail.com>
2026-06-29 10:38:48 +0300

    updated the use of functions

--- JS/jun-dev/intoFunction.js
+++ JS/jun-dev/intoFunction.js
@@ -1,5 +1,28 @@
 function timeAlert() {
   const date = new Date();
-  alert(`time stamp:${date.toISOString()}`);
+  //alert(`time stamp:${date.toISOString()}`);
 }
 timeAlert();
+
+function areaOfTriangle(base, height) {
+  console.log(`base is ${base} its type is ${typeof base}`);
+  console.log(`height is ${height} its type is ${typeof height}`);
+  const area = 0.5 * base * height;
+  console.log(
+    `for triangle with base ${base} and height ${height} area is ${area}`,
+  );
+}
+areaOfTriangle(5, 10);
+areaOfTriangle(20);
+areaOfTriangle(20, 30);
+
+let base1 = 50;
+let height1 = 60;
+
+areaOfTriangle(base1, height1);
+
+let base2 = "hello";
+let height2 = true;
+
+areaOfTriangle(base2, height2);
+console.log(typeof areaOfTriangle);
