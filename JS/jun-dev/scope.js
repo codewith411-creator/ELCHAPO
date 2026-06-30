ca1fdb0898edfc6d700503d8715fe992aef5ab75
RICHIMPZ <richiemwebi@gmail.com>
2026-06-30 15:44:03 +0300

    scope

---
+++ JS/jun-dev/scope.js
@@ -0,0 +1,11 @@
+let name = "Samson";
+function sayMyname() {
+  let name = "Maggy";
+  let name = "Moseti";
+  if (true) {
+    let name = "Jameson";
+    console.log(`name: ${name} line: 6`);
+  }
+  console.log(`name:${name} line: `);
+}
+sayMyname();
