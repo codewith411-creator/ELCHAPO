c991c73db6b6e7325976603e093fedeb66eca308
RICHIMPZ <richiemwebi@gmail.com>
2026-07-13 14:24:40 +0300

    create array is an object

---
+++ JS/basic-engineer/arrayIsObject.js
@@ -0,0 +1,10 @@
+const ar = [20, 5];
+
+const arObj = {
+  0: 20,
+  1: 5,
+  length: 2,
+};
+
+console.table(ar);
+console.table(arObj);
---
+++ JS/basic-engineer/arrays.js
@@ -0,0 +1,39 @@
+//
+//Creating an array
+//
+let carDetails = [
+  "KDL 123X",
+  "Toyota",
+  "RAV4",
+  2019,
+  "JTMDFRFV0KD01XXXX",
+  "2AR-1234567",
+  "Station Wagon / SUV",
+  "Petrol / Gasoline",
+  2494,
+  "Pearl White",
+  "Richie Mwebi",
+  "This car has never caused any accident and it's in good condition!",
+  [88, 4, 33, 73],
+];
+//
+//Access the array values
+//
+console.log(carDetails[0]);
+console.log(carDetails[1]);
+console.log(carDetails[2]);
+console.log(carDetails[3]);
+console.log(carDetails[4]);
+console.log(carDetails[5]);
+console.log(carDetails[6]);
+console.log(carDetails[7]);
+console.log(carDetails[8]);
+console.log(carDetails[9]);
+console.log(carDetails[10]);
+console.log(carDetails[11]);
+//
+//Loop the array
+//
+for (let i = 0; i < carDetails.length; i++) {
+  console.log(carDetails[i]);
+}
