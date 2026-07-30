83a1cd016fd2ea9c923d5bcb2cd9ca1aa36468b0
RICHIMPZ <richiemwebi@gmail.com>
2026-07-31 02:54:15 +0300

    Create Props for Child from Parenty

--- vite-project/src/Props/index.jsx
+++ vite-project/src/Props/index.jsx
@@ -1,23 +1,97 @@
 function Parent() {
+  // Primitive Data Types
+  const str = "Hello World";
+  const amount = 2000;
+  const isOk = true;
+  const notSet = null;
+
+  // Non-Primitive Data Types
+  const fruits = ["Apple", "Banana", "Orange"];
+
+  const student = {
+    name: "Rachel",
+    age: 22,
+    country: "Kenya",
+  };
+
+  function greet() {
+    alert("Hello from Parent!");
+  }
+
   return (
     <div>
-      <h1>I am the parent component</h1>
+      <h1>I am the Parent Component</h1>
 
-      <Child name="Rich" age={22} country="Kenya" />
+      <Child
+        str={str}
+        amountInNumber={amount}
+        isOk={isOk}
+        notSet={notSet}
+        fruits={fruits}
+        student={student}
+        greet={greet}
+      />
     </div>
   );
 }
+
+/* ---------------- CHILD COMPONENT ---------------- */
 
 function Child(props) {
   console.log(props);
 
   return (
-    <div>
-      <h1>I am the child component</h1>
+    <div
+      style={{
+        border: "2px solid gray",
+        padding: "20px",
+        marginTop: "20px",
+      }}
+    >
+      <h2>I am the Child Component</h2>
 
-      <p>Name: {props.name}</p>
-      <p>Age: {props.age}</p>
-      <p>Country: {props.country}</p>
+      <ul>
+        <li>
+          Favourite String: <b>{props.str}</b>
+        </li>
+
+        <li>
+          Amount: <b>{props.amountInNumber}</b>
+        </li>
+
+        <li>
+          Boolean: <b>{String(props.isOk)}</b>
+        </li>
+
+        <li>
+          Null Value: <b>{String(props.notSet)}</b>
+        </li>
+
+        <li>
+          Student Name: <b>{props.student.name}</b>
+        </li>
+
+        <li>
+          Student Age: <b>{props.student.age}</b>
+        </li>
+
+        <li>
+          Student Country: <b>{props.student.country}</b>
+        </li>
+
+        <li>
+          Fruits:
+          <ul>
+            {props.fruits.map((fruit) => (
+              <li key={fruit}>{fruit}</li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+
+      <button onClick={props.greet}>Click Me</button>
     </div>
   );
 }
+
+export default Parent;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,28 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Navigation from "./Navigation";
-import FilterList from "./FilterList";
-import ItemCard from "./ItemCard";
-
-import "./index.css";
+import Parent from "./Props";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>
-      <div className="content">
-        <div className="side-bar">
-          <FilterList />
-          <FilterList />
-          <FilterList />
-        </div>
-
-        <div className="products">
-          <ItemCard />
-          <ItemCard />
-          <ItemCard />
-        </div>
-      </div>
-    </>
+    <Parent />
   </StrictMode>,
 );
